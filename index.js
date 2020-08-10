39278cd7f58057d7e4b54c06fc4e7a9a5e0f01ca
Oskar Sherrah <n10261109@qut.edu.au>
2020-08-10 11:40:06 +1000

    Basic Hello World app for Netlify

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const chalk = require('chalk');
+// This is the entry point for the web server
 
-console.log("Running!");
+var express = require('express');
+var app = express();
+var port = 8080;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.send("Hello World!");
+});
+
+app.listen(port);
+console.log(`Server now running on port ${port}`);
